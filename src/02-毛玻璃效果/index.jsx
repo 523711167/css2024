8fda666947eba2683a88137764d8408c9879b1e8
pxx <523711167@qq.com>
2023-09-08 15:14:41 +0800

    文本填充效果

---
+++ src/02-毛玻璃效果/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/**
+ * 
+ * @Author pxx
+ * @DATA 2023/9/7 17:45
+ */
+ function Index() {
+    return (
+        <div>
+            
+        </div>
+    )
+ }
+    
+export default Index;
---
+++ src/03-文本填充效果/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './index.css'
+
+/**
+ *
+ * @Author pxx
+ * @DATA 2023/9/7 17:45
+ */
+function Index() {
+	return (
+		<>
+			<div className="container">
+				<div className="loader one">
+					<span></span>
+					<span></span>
+				</div>
+				<div className="loader two">
+					<span></span>
+					<span></span>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Index
