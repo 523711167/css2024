c8b1062a31f43b35c549fd09e915713f7e20e865
pxx <523711167@qq.com>
2023-09-10 00:03:26 +0800

    攀爬效果

---
+++ src/04-攀爬效果/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './index.css'
+
+/**
+ * 攀爬效果原理
+ * 1.固定右下角旋转
+ * 2.同时向后移动旋转带来的位移
+ * @Author pxx
+ * @DATA 2023/9/7 17:45
+ */
+function Index() {
+	return (
+		<>
+			<div className='container'>
+				<div className='box'>
+					<div className='cube'></div>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Index
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import Index from './01-旋转变色效果/index'
 // import Index from './02-毛玻璃效果/index'
-import Index from './03-文本填充效果/index'
+// import Index from './03-文本填充效果/index'
+import Index from './04-攀爬效果/index'
 
 
 function App() {
