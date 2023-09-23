c3cade45770fb611a5d4a57d305ee3c150fddef2
pxx <523711167@qq.com>
2023-09-23 18:44:43 +0800

    雷达扫描效果图

---
+++ src/15-雷达扫描效果图/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './index.css'
+/**
+ *
+ * @Author pxx
+ * @DATA 2023/9/23 16:57
+ */
+function Index() {
+	return (
+		<>
+			<ul className='radar'>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+		</>
+	)
+}
+
+export default Index
