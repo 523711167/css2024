5b654cef372c90606c7035a105122f9c6cd5e041
pxx <523711167@qq.com>
2023-10-05 19:33:51 +0800

    文本Css效果

---
+++ src/24-翻页文本css效果/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './index.css'
+/**
+ *
+ * @Author pxx
+ * @DATA 2023/10/5 19:14
+ */
+function Index() {
+	return (
+		<>
+      <ul>
+        <li>L</li>
+        <li>O</li>
+        <li>A</li>
+        <li>D</li>
+        <li>I</li>
+        <li>N</li>
+        <li>G</li>
+      </ul>
+		</>
+	)
+}
+
+export default Index
