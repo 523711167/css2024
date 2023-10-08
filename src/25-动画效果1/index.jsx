513e5d595038f0b966cefeef8fa04045adb166e9
pxx <523711167@qq.com>
2023-10-08 17:13:08 +0800

    不知道啥效果，感觉啥也不是

---
+++ src/25-动画效果1/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './index.css'
+/**
+ *
+ * @Author pxx
+ * @DATA 2023/10/8 16:15
+ */
+function Index() {
+	return (
+		<>
+			<div className='container'>
+				<div className='square'>
+					<div className='circle'></div>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Index
--- src/App.js
+++ src/App.js
@@ -19,7 +19,8 @@
 // import Index from './20-彩虹页面加载动画'
 // import Index from './22-简单文字跳动'
 // import Index from './23-小球跳动加载'
-import Index from './24-翻页文本css效果'
+// import Index from './24-翻页文本css效果'
+import Index from './25-动画效果1'
 
 
 function App() {
